2cbe521e8d0ed1a54b90d38509f92aa314dbecc1
vsVikramSingh <vsvikramsingh5200@gmail.com>
2023-11-21 06:07:12 +0530

    create first component

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,6 +1,9 @@
+import Test1 from "./test1";
+
+
 function App() {
   return (
-    <h1>Hello React</h1>
+    <Test1/>
   );
 }
 export default App;
---
+++ 01basicreact/src/test1.js
@@ -0,0 +1,7 @@
+function Test1(){
+    return(
+        <h3>Test1</h3>
+    )
+}
+
+export default Test1
